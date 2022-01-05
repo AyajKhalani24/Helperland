fdf1d1f08fb7bca894d384f287b464ba1e5a4dfa
Ayaj Khalani <khalaniayaj@gmail.com>
2022-01-05 18:25:47 +0530

    Service History

---
+++ HTML/Asset/upcomingservices.js
@@ -0,0 +1,41 @@
+document.querySelectorAll(".vertnavcont").forEach(ele => {
+    ele.addEventListener('click', () => {
+        document.querySelector('.current').classList.remove('current');
+        ele.classList.add('current');
+    });
+})
+const hamburger2=document.querySelector('.hamburger2');
+const opennav=document.querySelector('.usnav2');
+const closebutton=document.querySelector('.closebutton');
+
+hamburger2.addEventListener('click',()=>{
+    opennav.classList.add('open');
+})
+closebutton.addEventListener('click',()=>{
+    opennav.classList.remove('open')
+})
+
+
+
+$('.hamburger').click(() => {
+    $('.hamburger').toggleClass('toggle');
+    $('.usnav2').toggleClass('toggle');
+})
+
+const dt = new DataTable("#ustable", {
+    dom: "Rtlip",
+    responsive: true,
+    pagingType: "full_numbers",
+    language: {
+        paginate: {
+            first: "<img src='./images/firstpage.png' alt='first' />",
+            previous: "<img src='./images/previous.png' alt='previous' />",
+            next: '<img src="./images/previous.png" alt="next" style="transform: rotate(180deg)" />',
+            last: "<img src='./images/firstpage.png' alt='first' style='transform: rotate(180deg)' />",
+        },
+        info: "Total Record: _MAX_",
+        lengthMenu: "Show_MENU_Entries",
+    },
+    buttons: ["excel"],
+    columnDefs: [{ orderable: false, targets: 4 }],
+});
